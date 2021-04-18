f49d5ee514823d6d29a44bb09fec61c7cfe5a848
fgaudiel <francesco.gaudiello@capgemini.com>
2021-04-18 18:17:43 +0200

    Second NumPyDataScienceEssentialTraining_LinkedinLearning.py

--- Example/NumPyDataScienceEssentialTraining_LinkedinLearning.py
+++ Example/NumPyDataScienceEssentialTraining_LinkedinLearning.py
@@ -56,3 +56,108 @@ print(multizeros)
 print("***************")
 
 print("*** Index, Slice and Iterate ***")
+my_vector = np.array([-12, -4, 6, 5, 10, 32, 0, -5])
+print("My vector:\n", my_vector)
+print("my_vector[0]: ", my_vector[0])
+print("Change first element...")
+my_vector[0] = -50
+print("New my_vector:\n", my_vector)
+
+print("*** Two Dimensional Arrays ***")
+my_array = np.arange(35)    # Ho 35 elementi
+my_array.shape = (7, 5)     # Divisi in 7 righe e 5 colonne
+print("My_array:\n", my_array)
+
+# Access row or column
+print("my_array[2]: ", my_array[2])
+print("my_array[5, 2]: ", my_array[5, 2])
+
+print("\nAnother case to represent an element is")
+print("my_array[5][2]: ", my_array[5][2])
+print("***************")
+
+print("*** Three dimensional arrays ***")
+my_3D_array = np.arange(70)
+my_3D_array.shape = (2, 7, 5)   # 2 matrici di ognuna 7 righe e 5 colonne
+print("Il mio array 3D:\n", my_3D_array)
+
+print("\nmy_3D_array[1] -> take a matrix with index 1 (the second in this case):\n", my_3D_array[1])
+print("\nmy_3D_array[1, 3] -> take a matrix with index 1 and row 3 index with all column:\n", my_3D_array[1, 3])
+print("\nmy_3D_array[1, 3, 2] -> take a matrix with index 1 and row 3 index and column 2:\n", my_3D_array[1, 3, 2])
+print("Change element in [1, 3, 2]...")
+my_3D_array[1, 3, 2] = 1111
+print(my_3D_array)
+
+# Boolean Mask Array
+n = np.array([-7, -12, 4, 65, -76, 63, -6, 20, -5])
+print("Gli elementi dell'arrays ono divisibili per 7:")
+zero_mod_7_mask = 0 == (n % 7)
+print(zero_mod_7_mask)
+sub_array = n[zero_mod_7_mask]
+print("Il sub_array booleano:", sub_array)
+print("***************")
+
+# Numpy Logicla Operators
+mod_tes = 0 == (n % 7)
+positive_test = n > 0
+print("Positive_Test (solo positivi) = ", positive_test)
+
+combined_mask = np.logical_and(mod_tes, positive_test)
+print("Combined_mask (AND):", combined_mask)
+print(n[combined_mask])
+combined_mask_or = np.logical_or(mod_tes, positive_test)
+print("Combined_mask_or (OR):", combined_mask_or)
+print(n[combined_mask_or])
+print("***************")
+
+print("*** Broadcasting ***")
+x = np.arange(70)
+x.shape = (2, 7, 5)
+print("my_3d_array:\n", x)
+print("x.shape: ", x.shape)
+print("x.ndim: ", x.ndim)
+print("x.size: ", x.size)
+print("x.dtype: ", x.dtype)
+
+print(5 * x - 2)
+print("***************")
+
+left_mat = np.arange(6).reshape((2, 3))
+right_mat = np.arange(15).reshape((3, 5))
+print("Dot:", np.dot(left_mat, right_mat))
+
+print("*** Operations along axes ***")
+print("x.shape: ", x.shape)
+print("x.sum(): ", x.sum())
+print("x.sum(axis=0):\n", x.sum(axis=0))     # axis=0 = colonne
+print("x.sum(axis=1):\n", x.sum(axis=1))     # axis=1 = righe
+print("***************")
+
+print("*** Creaing Structured Arrays ***")
+person_data_def = [('name', 'S5'), ('height', 'f8'), ('weight', 'f8'), ('age', 'i8')]
+print("Person_data_def:", person_data_def)
+
+people_array = np.zeros(4, dtype=person_data_def)
+print("People_array:", people_array)
+people_array[3] = ('Delta', 73, 205, 34)
+print("People_array:", people_array)
+people_array[0] = ('Alfa', 65, 112, 23)
+print("People_array:", people_array)
+ages = people_array['age']
+print("Ritorna 'age' di tutte le persone sotto forma di array: ", ages)
+print("***************")
+
+print("*** Multi-dimensional Structured Arrays ***")
+people_big_array = np.zeros((4, 3, 2), dtype=person_data_def)
+print("people_big_array:\n", people_big_array)
+people_big_array[3, 2, 1] = ('Echo', 68, 155, 46)
+print("***************")
+print("people_big_array:\n", people_big_array)
+print("***************")
+
+print("people_big_array['height']:\n", people_big_array['height'])
+print("***************")
+
+print("*** Creating Record Array ***")
+person_record_array = np.rec.array([('Delta', 54, 65, 12), ('John', 96, 143, 9)], dtype=person_data_def)
+print("person_record_array:\n", person_record_array)
