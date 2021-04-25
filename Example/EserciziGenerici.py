5b27508b99edae9002e7041f1eeebc59c4d90a85
FrancescoGaudiello10 <francesco.gaudiello.1995@gmail.com>
2021-04-25 15:57:14 +0200

    EserciziGenerici pt.2

--- Example/EserciziGenerici.py
+++ Example/EserciziGenerici.py
@@ -114,3 +114,208 @@ print(a.T.shape)
 print("array a:\n", a)
 a.resize((2, 6))
 print("array after resize:\n", a)
+
+print("*** STACKING TOGETHER DIFFERENT ***")
+print("m:\n", m)
+print("n:\n", n)
+print("********")
+mn_vstack = np.vstack((m, n))
+print(mn_vstack)
+print("********")
+mn_hstack = np.hstack((m, n))
+print(mn_hstack)
+print("********")
+
+# columns_stack inserisce in sotto una colonna unificata (valido solo per 1D)
+mn_columnstack = np.column_stack((m, n))
+print(mn_columnstack)
+a = np.array([1, 2])
+b = np.array([5, 7])
+print("********")
+ab_columnstack = np.column_stack((a, b))
+print(ab_columnstack)
+
+print("*** NO COPY AT ALL ***")
+a = np.array([[0, 1, 2, 3],
+              [4, 5, 6, 7],
+              [8, 9, 10, 12]])
+b = a                       # no new object is created
+print("b is a:", b is a)    # a and b are two names for the same ndarray object
+
+
+def f(x):
+    print(id(x))
+
+
+# Posso notare che puntano allo stesso indirizzo di mem.
+print("id(a):", id(a))
+print("id(b):", id(b))
+
+# View or shallow copy
+# view() crea un nuovo array object con gli stessi data
+c = a.view()
+print("c is a:", c is a)
+print("id(a):", id(a))
+print("id(c):", id(c))
+c = c.reshape((2, 6))
+print(a.shape)
+print(c.shape)
+
+# Noto che modificando un data di "c"
+# viene modificato anche "a"
+c[0, 4] = 12345
+print(a)
+
+# DEEP COPY
+# The copy method makes a complete copy of the array and its data.
+d = a.copy()
+print(d is a)
+print(d.base is a)
+print("d:\n", d)
+print("a:\n", a)
+print("***** modified d[0,0]...")
+d[0, 0] = 9999      # d doesn't share anything with a
+print("d:\n", d)
+print("a:\n", a)
+
+# example utility copy
+a = np.arange(int(1e8))
+b = a[:100].copy()
+del a                       # the memory of ``a`` can be released
+
+print("*** Functions and Methods Overview ***")
+"""
+List of some useful Numpy function: 
+Array Creation:
+    arange, array, copy, empty, empty_like, eye, fromfile, 
+    fromfunction, identity, linspace, logspace, mgrid, ogrid, 
+    ones, ones_like, r_, zeros, zeros_like
+
+Conversions:
+    ndarray.astype, atleast_1d, atleast_2d, atleast_3d, mat
+
+Manipulations:
+    array_split, column_stack, concatenate, diagonal, dsplit, 
+    dstack, hsplit, hstack, ndarray.item, newaxis, ravel, 
+    repeat, reshape, resize, squeeze, swapaxes, take, 
+    transpose, vsplit, vstack
+
+Questions:
+    all, any, nonzero, where
+
+Ordering:
+    argmax, argmin, argsort, max, min, ptp, searchsorted, sort
+
+Operations:
+    choose, compress, cumprod, cumsum, inner, 
+    ndarray.fill, imag, prod, put, putmask, real, sum
+
+Basic Statistics:
+    cov, mean, std, var
+
+Basic Linear Algebra:
+    cross, dot, outer, linalg.svd, vdot
+"""
+
+print("*** LESS BASIC ***")
+print("Bradcasting rules")
+# Broadcasting allows universal functions to deal in a meaningful
+# way with inputs that do not have exactly the same shape.
+# The first rule of broadcasting is that if all input arrays
+# do not have the same number of dimensions, a “1” will be
+# repeatedly prepended to the shapes of the smaller arrays until
+# all the arrays have the same number of dimensions.
+#
+# The second rule of broadcasting ensures that arrays
+# with a size of 1 along a particular dimension act as
+# if they had the size of the array with the largest shape along that dimension.
+# The value of the array element is assumed to be the same along that
+# dimension for the “broadcast” array.
+# Rif: https://numpy.org/doc/stable/user/basics.broadcasting.html#basics-broadcasting
+
+print("*** Advanced indexing and index tricks ***")
+a = np.arange(12)**2
+print("a:\n", a)
+i = np.array([1, 1, 3, 8, 5])
+print("i:\n", i)
+print("a[i]:", a[i])        # Stampa gli elementi secondo l'indice "i" presenti in "a"
+
+j = np.array([[3, 4],
+              [9, 7]])
+print("j:\n", j)
+print("a[j]:\n", a[j])      # Eleva al quadrato gli elementi di j
+print("************")
+
+print("*** PALETTE ***")
+palette = np.array([[0, 0, 0],
+                    [255, 0, 0],
+                    [0, 255, 0],
+                    [0, 0, 255],
+                    [255, 255, 255]])
+print("palette:\n", palette)
+image = np.array([[0, 1, 2, 0],
+                  [0, 3, 4, 0]])
+print("image:\n", image)
+print("palette[image]:\n", palette[image])
+
+a = np.arange(12).reshape(3, 4)
+print("a:\n", a)
+
+i = np.array([[0, 1],
+              [1, 2]])
+j = np.array([[2, 1],
+              [3, 3]])
+print("i:\n", i)
+print("j:\n", j)
+print("a[i, j]:\n", a[i, j])
+print("********")
+
+a = np.arange(12).reshape(3, 4)
+print("a:\n", a)
+i = np.array([[0, 1],
+              [1, 2]])
+j = np.array([[2, 1],
+              [3, 3]])
+print("i:\n", i)
+print("j:\n", j)
+
+print("a[i, j]:\n", a[i, j])
+print("*********")
+
+print("*** Indexing with Boolean Arrays ***")
+a = np.arange(12).reshape(3, 4)
+b = a > 4
+print(b)        # Array Booleano di true/false
+print(a[b])     # Stampa i soli valori TRUE
+a[b] = 0        # All elements of 'a' higher than 4 become 0
+print(a)
+
+print("*********")
+print("*** Mandelbrot set ***")
+import matplotlib.pyplot as plt
+def mandelbrot(h, w, maxit=20):
+    """ RETURNS AN IMAGE OF THE MANDELBROT FRACTAL OF SIZE (H,W) """
+    y, x = np.ogrid[-1.4:1.4:h*1j, -2:0.8:w*1j]
+    c = x+y*1j
+    z = c
+    divtime = maxit + np.zeros(z.shape, dtype=int)
+
+    for i in range(maxit):
+        z = z**2 + c
+        diverge = z*np.conj(z) > 2**2
+        div_now = diverge & (divtime == maxit)
+        divtime[div_now] = i
+        z[diverge] = 2
+    return divtime
+
+#plt.imshow(mandelbrot(400, 400))
+
+# second way to index with boolena
+a = np.arange(12).reshape(3, 4)
+b1 = np.array([[False, True, True]])
+b2 = np.array([True, False, True, False])
+
+# print("a[b1, :]:\n", [a[b1, :]])
+print("************")
+print("*** TRICKS AND TIPS ***")
+print("Automatic Reshaping")
